69e7b3832480e9091870979c4fdd531c7a0d6c33
deep bahirshet <deepbahirshet@gmail.com>
2025-11-03 21:03:18 +0530

    Added all project files

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { PhotoIcon } from "@heroicons/vue/24/solid";
+import { ref } from "vue";
+import axiosClient from "../axios";
+import router from '../router';
+
+const imageData = ref({
+    'label': '',
+    'image': ''
+})
+
+function submit(){
+    const formData = new FormData();
+    formData.append('image', imageData.value.image);
+    formData.append('label', imageData.value.label);
+    axiosClient.post('/api/image', formData)
+    .then(res => {
+        router.push({name: "MyImages"});
+    })
+}
+
+</script>
+
+<template>
+    <header
+        class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-white">Upload</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+           <form @submit.prevent="submit">
+             <div class="mb-4">
+                <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
+                <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                    <div class="text-center">
+                        <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
+                        <div class="mt-4 flex text-sm/6 text-gray-600">
+                            <label for="file-upload"
+                                class="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500">
+                                <span>Upload a file</span>
+                                <input @change="imageData.image = $event.target.files[0]" id="file-upload" name="file-upload" type="file" class="sr-only" />
+                            </label>
+                            <p class="pl-1">or drag and drop</p>
+                        </div>
+                        <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                    </div>
+                </div>
+            </div>
+           <div class="mb-3">
+            <label for="label" class="block text-sm/6 font-medium text-gray-900">Label</label>
+            <div class="mt-2">
+              <input v-model="imageData.label" type="text" name="label" id="label" autocomplete="given-name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            </div>
+          </div>
+            <button type="submit"
+                class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                Upload
+            </button>
+           </form>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
